90e6008f7fcf87f59d081d90cc432a2786a630a0
shuva79 <shuvabro@gmail.com>
2024-04-14 16:45:26 +0545

    deleted:    actual.py 	deleted:    actual1.py 	deleted:    enter_symbol.py deleted:    home.html 	deleted:    local_time.py 	deleted:    login.py 	deleted:    main.py 	deleted:    pull.py 	deleted:    request.py 	deleted:    screenshot.py 	deleted:    test.py 	deleted:    yessai/java_pseudocode_vari.py 	deleted:    yessai/java_stuff.py 	deleted:    yessai/java_stuff.txt 	deleted:    yessai/java_stuff_.txt 	deleted:    yessai/java_stuff_pesudocode.py

--- pull.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver  
-# To perform automated user actions, we need a dedicated library for the respective browser
-# In this case, we use webdriver library for chrome
-
-driver = webdriver.Chrome()
-# Note: You need a chromedriver exe along with webdriver for this to work
